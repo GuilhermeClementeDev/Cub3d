dfcde8de1d64e79c56051a54f94765903eb43d19
yfaustin <ytower@proton.me>
2025-10-31 17:08:32 -0300

    norminette

--- src/engine.c
+++ src/engine.c
@@ -1,10 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   engine.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yfaustin <yfaustin@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/31 17:07:21 by yfaustin          #+#    #+#             */
+/*   Updated: 2025/10/31 17:08:20 by yfaustin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "cub3d.h"
 
 int	main_loop(t_game *game)
 {
 	handle_player_movement(game);
 	ft_raycasting(game);
-	mlx_put_image_to_window(game->mlx, game->win, \
-		game->background.img, 0, 0);
+	mlx_put_image_to_window(game->mlx, game->win, game->background.img, 0, 0);
 	return (0);
 }
