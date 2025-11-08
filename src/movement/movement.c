fbf28f764f5718db9503852adf94238e8d913669
yfaustin <ytower@proton.me>
2025-11-08 05:43:54 -0300

    norminette

--- src/movement/movement.c
+++ src/movement/movement.c
@@ -6,7 +6,7 @@
 /*   By: yfaustin <yfaustin@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/31 17:12:08 by yfaustin          #+#    #+#             */
-/*   Updated: 2025/10/31 17:13:26 by yfaustin         ###   ########.fr       */
+/*   Updated: 2025/11/08 05:38:47 by yfaustin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,14 @@ static void	rotate_player(t_player *player, double rot_speed)
 	double	old_plane_x;
 
 	old_dir_x = player->dir_x;
-	player->dir_x = player->dir_x * cos(rot_speed) - player->dir_y * sin(rot_speed);
+	player->dir_x = player->dir_x * cos(rot_speed) - player->dir_y
+		* sin(rot_speed);
 	player->dir_y = old_dir_x * sin(rot_speed) + player->dir_y * cos(rot_speed);
 	old_plane_x = player->plane_x;
-	player->plane_x = player->plane_x * cos(rot_speed) - player->plane_y * sin(rot_speed);
-	player->plane_y = old_plane_x * sin(rot_speed) + player->plane_y * cos(rot_speed);
+	player->plane_x = player->plane_x * cos(rot_speed) - player->plane_y
+		* sin(rot_speed);
+	player->plane_y = old_plane_x * sin(rot_speed) + player->plane_y
+		* cos(rot_speed);
 }
 
 static void	apply_movement(t_game *game, double move_x, double move_y)
@@ -31,28 +34,41 @@ static void	apply_movement(t_game *game, double move_x, double move_y)
 	t_player	*p;
 	double		new_pos_x;
 	double		new_pos_y;
-	int			new_map_x;
-	int			new_map_y;
 
 	p = &game->map_game.player;
 	new_pos_x = p->width + move_x;
 	new_pos_y = p->height + move_y;
-	new_map_y = (int)new_pos_y;
-	new_map_x = (int)new_pos_x;
-	if (new_map_y >= 0 && new_map_y < game->map_game.height
-		&& game->map_game.map[new_map_y]
-		&& new_map_x >= 0 && new_map_x < (int)ft_strlen(game->map_game.map[new_map_y])
-		&& game->map_game.map[new_map_y][new_map_x] != '1')
+	update_x_position(game, new_pos_x, new_pos_y);
+	update_y_position(game, new_pos_y);
+}
+
+static void	calculate_movement(t_game *game, double move_speed,
+		double *move_x, double *move_y)
+{
+	t_player	*p;
+
+	p = &game->map_game.player;
+	*move_x = 0;
+	*move_y = 0;
+	if (game->keys.w)
 	{
-		p->width = new_pos_x;
+		*move_x += p->dir_x * move_speed;
+		*move_y += p->dir_y * move_speed;
 	}
-	new_map_x = (int)p->width;
-	if (new_map_y >= 0 && new_map_y < game->map_game.height
-		&& game->map_game.map[new_map_y]
-		&& new_map_x >= 0 && new_map_x < (int)ft_strlen(game->map_game.map[new_map_y])
-		&& game->map_game.map[new_map_y][new_map_x] != '1')
+	if (game->keys.s)
 	{
-		p->height = new_pos_y;
+		*move_x -= p->dir_x * move_speed;
+		*move_y -= p->dir_y * move_speed;
+	}
+	if (game->keys.d)
+	{
+		*move_x += p->plane_x * move_speed;
+		*move_y += p->plane_y * move_speed;
+	}
+	if (game->keys.a)
+	{
+		*move_x -= p->plane_x * move_speed;
+		*move_y -= p->plane_y * move_speed;
 	}
 }
 
@@ -67,28 +83,7 @@ void	handle_player_movement(t_game *game)
 	p = &game->map_game.player;
 	move_speed = MOVE_SPEED;
 	rot_speed = ROT_SPEED;
-	move_x = 0;
-	move_y = 0;
-	if (game->keys.w)
-	{
-		move_x += p->dir_x * move_speed;
-		move_y += p->dir_y * move_speed;
-	}
-	if (game->keys.s)
-	{
-		move_x -= p->dir_x * move_speed;
-		move_y -= p->dir_y * move_speed;
-	}
-	if (game->keys.d)
-	{
-		move_x += p->plane_x * move_speed;
-		move_y += p->plane_y * move_speed;
-	}
-	if (game->keys.a)
-	{
-		move_x -= p->plane_x * move_speed;
-		move_y -= p->plane_y * move_speed;
-	}
+	calculate_movement(game, move_speed, &move_x, &move_y);
 	if (move_x != 0 || move_y != 0)
 		apply_movement(game, move_x, move_y);
 	if (game->keys.right)
